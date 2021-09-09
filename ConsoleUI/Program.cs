d4b09093a681e4506616245e29324debb3da9071
Snailware <Adamlancaster.al@gmail.com>
2021-09-08 20:52:23 -0400

    testing

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -72,80 +72,20 @@ namespace ConsoleUI
 
 			void CallAboutMenu()
 			{
-				string[] aboutMenu =
-				{
-					@"                       .                                           ,       ", // 1
-					@"        .                         ,            .              .            ",
-					@" ,                .                 .                                      ",
-					@"         .                 *                ,          .                   ",
-					@"              ,                                                     .      ", // 5
-					@"    *    This is sample text. this is sample text. this is sample          ",
-					@"  .                   .       ,                 .                     .    ",
-					@"      .  This is sample text. this is sample text. this is sample          ",
-					@"       ,                                 ,                       .         ",
-					@"         This is sample text. this is sample text. this is sample       *  ", // 10
-					@"    .                    .         .                   ,             ,     ",
-					@"         This is sample text. this is sample text. this is sample          ",
-					@",                .                    ,                     .           .  ",
-					@"                       .                                           ,       ",
-					@"        .                         ,            .       *      .            ", // 15
-					@" ,                .                 .                                      ",
-					@"         .                 *                ,          .                   ",
-					@"              ,                                                     .      ",
-					@"       .                                           ,                       ",
-					@"                                .                            .             ", // 20
-					@"             .        ,                    .                               ",
-					@"    ,                                             ,                  ,     ",
-					@"+-------------------------------------------------------------------------+",
-					@"|                                                                         |",
-					@"|                         press [ENTER] to return                         |", // 25
-					@"|                                                                         |",
-					@"+-------------------------------------------------------------------------+"  // 27
-				};
-				// about menu array.
+				string line1 = "Fight hostile aliens to liberate occupied planets. Save                     ",
+					   line2 = "as many worlds as possible to earn a high score!",
+					   line6 = "made by Adam Lancaster, Tracey Pinckney, Clarence Dews",
+					   prompt = "press [ENTER] to return";
+				// info to display.
 
-				string textChars = "abcdefghijklmnopqrstuvwxyz" + 
-					               "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-				// characters to be highlighted as text.
-
-				for (int index = 0; index < aboutMenu.Length; index++)
-				{
-					foreach (char character in aboutMenu[index])
-					{
-						if (textChars.Contains(character) && 
-							index < 23)
-						{
-							Console.ForegroundColor = ConsoleColor.Green;
-							Console.Write(character);
-							Console.ResetColor();
-						}
-						// display text in special color.
-
-						else if (character == '*')
-						{
-							Console.ForegroundColor = ConsoleColor.Yellow;
-							Console.Write(character);
-							Console.ResetColor();
-						}
-						// display * in special color. 
-
-						else
-						{
-							Console.Write(character);
-						}
-						// display other text normally.
-					}
-					// display certain chars with styling.
-
-					Console.WriteLine();
-					// write eol.
-				}
-				// display about menu with styling.
-
-				Console.ReadLine();
-				// wait for input.
+				CallDynamicMenu(line1: line1,
+								line2: line2,
+								line6: line6,
+								prompt: prompt);
+				// display dynamic menu and wait for ENTER.
 
 				RunStartMenuLoop();
+				// return to start menu.
 			}
 			// display about menu & wait for response.
 
@@ -196,14 +136,25 @@ namespace ConsoleUI
 							Console.ForegroundColor = ConsoleColor.DarkRed;
 							Console.Write(character);
 							Console.ResetColor();
-						} 
-						else 
+						}
+						else if (character == '#') 
+						{
+							Console.ForegroundColor = ConsoleColor.DarkYellow;
+							Console.Write(character);
+							Console.ResetColor();
+						}
+						else if (character == '^')
+						{
+							Console.ForegroundColor = ConsoleColor.Green;
+							Console.Write(character);
+							Console.ResetColor();
+						}
+						else
 						{
 							Console.Write(character);
 						}
 					}
-					// display title chars in special color. display others
-					// normally.
+					// display characters in different colors.
 
 					Console.WriteLine();
 					// write eol.
@@ -215,12 +166,166 @@ namespace ConsoleUI
 			}
 			// display start menu & return user input.
 
-			bool CallCharCreationMenu()
+			string CallCharCreationMenu()
 			{
+				string charName = "",
+					   charPassword = "",
+					   charRace = "",
+					   charClass = "";
+				// character attribute vars.
 
+				bool containsCapital = false,
+					 containsLowercase = false,
+					 containsSpecialChar = false;
+				// valid password flags.
 
-		
+				charName = CallDynamicMenu(line1: $"NAME: {charName}",
+										   line2: $"PASSWORD: {charPassword}",
+										   line3: $"RACE: {charRace}",
+										   line4: $"CLASS: {charClass}",
+										   prompt: "enter NAME");
+				// get name input.
+
+				do
+				{
+					charPassword = CallDynamicMenu(line1: $"NAME: {charName}",
+												   line2: $"PASSWORD: {charPassword}",
+												   line3: $"RACE: {charRace}",
+												   line4: $"CLASS: {charClass}",
+												   prompt: "enter PASSWORD with atleast 1 cap, 1 lowercase, & 1 special.");
+					// get password input.
+
+					foreach (char character in charPassword)
+					{
+						if (char.IsUpper(character))
+						{
+							containsCapital = true;
+						}
+						else if (char.IsLower(character))
+						{
+							containsLowercase = true;
+						}
+						else if (!char.IsLetterOrDigit(character))
+						{
+							containsSpecialChar = true;
+						}
+					}
+					// check password for validity and set flags accordingly.
+
+				} while (!containsCapital ||
+						 !containsLowercase ||
+						 !containsSpecialChar);
+				// get validated password input.
+
+				charRace = CallDynamicMenu(line1: $"NAME: {charName}",
+										   line2: $"PASSWORD: {charPassword}",
+						                   line3: $"RACE: {charRace}",
+						                   line4: $"CLASS: {charClass}",
+						                   prompt: "enter RACE");
+				// get race input.
+
+				charClass = CallDynamicMenu(line1: $"NAME: {charName}",
+											line2: $"PASSWORD: {charPassword}",
+											line3: $"RACE: {charRace}",
+											line4: $"CLASS: {charClass}",
+											prompt: "enter CLASS");
+				// get class input.
 			}
+			// display character creation menu and return user input.
+
+			string CallDynamicMenu(string line1 = "",
+								   string line2 = "",
+								   string line3 = "",
+								   string line4 = "",
+								   string line5 = "",
+								   string line6 = "",
+								   string line7 = "",
+								   string prompt = "")
+			{ 
+				if (line1.Length > 57 || 
+					line2.Length > 57 ||
+					line3.Length > 57 ||
+					line4.Length > 57 ||
+					line5.Length > 57 ||
+					line6.Length > 57 ||
+					line7.Length > 57 ||
+					prompt.Length > 57)
+				{
+					throw new Exception("an arg exceeds char limit of 57.");
+				}
+				// throw exception if any args exceed intended length.
+
+				line1 = String.Format("{0, -57}", line1);
+				line2 = String.Format("{0, -57}", line2);
+				line3 = String.Format("{0, -57}", line3);
+				line4 = String.Format("{0, -57}", line4);
+				line5 = String.Format("{0, -57}", line5);
+				line6 = String.Format("{0, -57}", line6);
+				line7 = String.Format("{0, -57}", line7);
+				prompt = String.Format("{0, -57}", prompt);
+				// format all strings for appropriate viewing.
+
+				string[] dynamicMenu =
+				{
+					@"                       .                                           ,       ", // 1
+					@"        .                         ,            .              .            ",
+					@" ,                .                 .                                      ",
+					@"         .                 *                ,          .                   ",
+					@"              ,                                                     .      ", // 5
+					$"    *    {line1}         ",
+					@"  .                   .       ,                 .                     .    ",
+					$"      .  {line2}         ",
+					@"       ,                                 ,                       .         ",
+					$"         {line3}      *  ",													// 10
+					@"    .                    .         .                   ,             ,     ",
+					$"         {line4}         ",
+					@",                .                    ,                     .           .  ",
+					$"         {line5}         ",
+					@"        .                         ,            .       *      .            ", // 15
+					$" ,       {line6}   ,     ",
+					@"         .                 *                ,          .                   ",
+					$"         {line7}     ,   ",
+					@"       .                                           ,                       ",
+					@"                                .                            .             ", // 20
+					@"             .        ,                    .                               ",
+					@"    ,                                             ,                  ,     ",
+					@"+-------------------------------------------------------------------------+",
+					@"|                                                                         |",
+					$"|        {prompt}        |",													// 25
+					@"|                                                                         |",
+					@"+-------------------------------------------------------------------------+"  // 27
+				};
+				// menu to display. 
+
+				foreach (string line in dynamicMenu)
+				{
+					foreach (char character in line)
+					{
+						if (char.IsLetterOrDigit(character))
+						{
+							Console.ForegroundColor = ConsoleColor.Green;
+						}
+						else if (character == '*')
+						{
+							Console.ForegroundColor = ConsoleColor.DarkYellow;
+						}
+						// set color for character.
+
+						Console.Write(character);
+						Console.ResetColor();
+						// write character and reset color for next iter.
+					}
+					// write styled line of menu.
+
+					Console.WriteLine();
+					// write eol.
+				}
+				// display dynamic menu with styling.
+
+				return Console.ReadLine().ToLower();
+				// get input, format as lowercase and return.
+			}
+			// display dynamic menu with desired info and prompt.
 
 			StartGame();
 		}
