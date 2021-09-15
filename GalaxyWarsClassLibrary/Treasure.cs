23ca8d2b8f3254c44d6286e39f2a1a78a9d43251
TraceyP89 <71043578+TraceyP89@users.noreply.github.com>
2021-09-15 19:42:22 -0400

    Added classes

---
+++ GalaxyWarsClassLibrary/Treasure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyWarsClassLibrary
+{
+    public class Treasure : Item
+    {
+        private int _value;
+
+        public Treasure(string name, string description, int value)
+            : base(name, description)
+        {
+            _name = name;
+            _description = description;
+            _value = value;
+        }
+        public int Value
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ GalaxyWarsClassLibrary/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyWarsClassLibrary
+{
+    public class Weapon : Item
+    {
+        private int _damage;
+        
+    
+        /// <summary>
+        /// weapon class derived from item
+        /// </summary>
+        /// <param name="name">name of the weapon</param>
+        /// <param name="description">description of the weapon</param>
+        /// <param name="damage">amount of damage</param>
+        public Weapon(string name, string description, int damage)
+            : base(name, description)
+        {
+            _name = name;
+            _description = description;
+            _damage = damage;
+            
+        }
+        /// <summary>
+        /// Amount of damage
+        /// </summary>
+        public int Damage
+        {
+            get { return _damage; }
+        }
+    }
+}
