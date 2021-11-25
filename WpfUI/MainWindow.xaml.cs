145d172bbe6b914b9006ae27ac7630a3a9fa6693
Snailware <Adamlancaster.al@gmail.com>
2021-11-24 21:10:45 -0500

    created UI.

---
+++ WpfUI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonSubmit_Click(object sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		/// <summary>
+		/// on click, clear input text box.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ButtonClear_Click(object sender, RoutedEventArgs e)
+		{
+			InputTextBox.Clear();
+		}
+
+		/// <summary>
+		/// on click, close program.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ButtonExit_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
